18c77dbdbf3bde2d814996ad0ba8edf71dec1927
Herobrine25mcpe <imherobrine25@gmail.com>
2023-03-26 19:49:14 +0530

    intro

--- src/component/Intro/Intro.jsx
+++ src/component/Intro/Intro.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Intro.css'
-import About from '../../img/about.png'
+import About from '../../img/about.gif'
 import { useContext } from "react";
 import { themeContext } from "../../Context";
 import { Link } from "react-scroll";
@@ -16,18 +16,18 @@ const Intro =()=>{
             <div className='i-left'>
 
                 <div className='i-name'>
-                    <span style={{ color: darkMode ? "white" : "" }}> Hey I am </span>
-                    <span> Tarun Singh</span>                    
-                    <span> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi, adipisci. Harum beatae, asperiores at architecto dicta adipisci sit possimus omnis magni expedita sunt odit, exercitationem amet dolore ipsa eligendi. Sunt.</span> 
+                    <span style={{ color: darkMode ? "#eb2324" : "" }}>Welcome </span>
+                    <span> Developers</span>                    
+                    <span> Our website provides comprehensive and up-to-date Java cheat sheets to help Java developers quickly find the information they need. Our cheat sheets cover syntax, data types, operators, loops, arrays, and more, making them easy to read and understand.</span> 
                 </div>
                 <button className=' button i-button'>
-                <Link activeClass="active" to="Contact" spy={true} smooth={true}>Contact</Link>
+                <Link activeClass="active" to="Contact" spy={true} smooth={true}>Let's Go</Link>
                 </button>
             </div>
             <div className='i-right'>
                 <div className='i-image'>
                     
-                    <img src={About} alt="" />
+                    <img src="" alt="" />
                 </div>
                 
             </div>
