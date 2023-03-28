695ad09519c9bd9a0477f3b50175e54e0732e82a
Vansh <vansh8755381346@gmail.com>
2023-03-28 21:43:07 +0530

    aos

--- src/component/Intro/Intro.jsx
+++ src/component/Intro/Intro.jsx
@@ -1,35 +1,36 @@
 import React from 'react'
 import './Intro.css'
-import About from '../../img/about.gif'
+// import About from '../../img/about.gif'
 import { useContext } from "react";
 import { themeContext } from "../../Context";
 import { Link } from "react-scroll";
 
 
-const Intro =()=>{
 
+const Intro = () => {
 
     const theme = useContext(themeContext);
     const darkMode = theme.state.darkMode;
-    return(
+    return (
         <div className='intro'>
-            <div className='i-left'>
+            <div className='i-left'
+                data-aos="zoom-out-right">
 
                 <div className='i-name'>
                     <span style={{ color: darkMode ? "#C3073F" : "" }}>Welcome </span>
-                    <span> Developers</span>                    
-                    <span> Our website provides comprehensive and up-to-date Java cheat sheets to help Java developers quickly find the information they need. Our cheat sheets cover syntax, data types, operators, loops, arrays, and more, making them easy to read and understand.</span> 
+                    <span> Developers</span>
+                    <span> Our website provides comprehensive and up-to-date Java cheat sheets to help Java developers quickly find the information they need. Our cheat sheets cover syntax, data types, operators, loops, arrays, and more, making them easy to read and understand.</span>
                 </div>
                 <button className=' button i-button'>
-                <Link activeClass="active" to="Contact" spy={true} smooth={true}>Let's Go</Link>
+                    <Link activeClass="active" to="Contact" spy={true} smooth={true}>Let's Go</Link>
                 </button>
             </div>
             <div className='i-right'>
                 <div className='i-image'>
-                    
+
                     <img src="" alt="" />
                 </div>
-                
+
             </div>
         </div>
     )
--- src/component/Services/Services.jsx
+++ src/component/Services/Services.jsx
@@ -6,68 +6,75 @@ import resume from "./Tarun Singh_Resume.pdf";
 import { useContext } from "react";
 import { themeContext } from "../../Context";
 
-const Services = () =>{
+const Services = () => {
     const theme = useContext(themeContext);
     const darkMode = theme.state.darkMode;
 
-    return(
+    return (
 
         <div className="services" >
-            <div className="s-right">
+            <div className="s-right"
+                data-aos="zoom-out-right">
                 <span style={{ color: darkMode ? "white" : "" }}> Services </span>
-                <span> Lorem ipsum dolor sit amet, consectetur adipisicing elit.<br/> Incidunt minima quia ex eos mollitia porro quam ab adipisci quae su.<br/>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</span>
+                <span> Lorem ipsum dolor sit amet, consectetur adipisicing elit.<br /> Incidunt minima quia ex eos mollitia porro quam ab adipisci quae su.<br />Lorem ipsum dolor sit amet, consectetur adipisicing elit.</span>
                 <a href={resume} download> <button className="button s-button"> Resume </button></a>
-                
+
             </div>
-            <div className="cards"> 
-                <div style={{top: '-4rem', left: "12rem"}}>
+            <div className="cards">
+                <div style={{ top: '-4rem', left: "12rem" }}
+                    data-aos="zoom-out-left">
                     <Card
-                    image={vector1}
-                    heading = {'Data Science'}
-                    details = {"lorem ipsum dolor sit amet"}
+                        image={vector1}
+                        heading={'Data Science'}
+                        details={"lorem ipsum dolor sit amet"}
                     />
                 </div>
 
-                <div style={{top: "11rem" ,left: "-9rem"}}>
+                <div style={{ top: "11rem", left: "-9rem" }}
+                    data-aos="zoom-out-right">
                     <Card
-                    image={vector1}
-                    heading = {'Data Science'}
-                    details = {"lorem ipsum dolor sit amet"}
+                        image={vector1}
+                        heading={'Data Science'}
+                        details={"lorem ipsum dolor sit amet"}
                     />
                 </div>
 
-                <div style={{top: "16rem" ,left: "-24rem"}}>
+                <div style={{ top: "16rem", left: "-24rem" }}
+                    data-aos="zoom-out-right">
                     <Card
-                    image={vector1}
-                    heading = {'Data Science'}
-                    details = {"lorem ipsum dolor sit amet"}
+                        image={vector1}
+                        heading={'Data Science'}
+                        details={"lorem ipsum dolor sit amet"}
                     />
                 </div>
 
-                <div style={{top: "14rem" ,left: "25rem"}}>
+                <div style={{ top: "14rem", left: "25rem" }}
+                    data-aos="zoom-out-left">
                     <Card
-                    image={vector1}
-                    heading = {'Data Science'}
-                    details = {"lorem ipsum dolor sit amet"}
-                    />
-                </div>
-                
-                <div Id='Services'style={{top: "-7rem" ,left: "28rem"}}>
-                    <Card
-                    image={vector1}
-                    heading = {'Data Science'}
-                    details = {"lorem ipsum dolor sit amet"}
+                        image={vector1}
+                        heading={'Data Science'}
+                        details={"lorem ipsum dolor sit amet"}
                     />
                 </div>
 
-                <div style={{top: '15rem',left: '6rem'}}>
+                <div Id='Services' style={{ top: "-7rem", left: "28rem" }}
+                    data-aos="zoom-out-left">
                     <Card
-                    image={vector1}
-                    heading = {'Data Science'}
-                    details = {"lorem ipsum dolor sit amet"}
+                        image={vector1}
+                        heading={'Data Science'}
+                        details={"lorem ipsum dolor sit amet"}
                     />
                 </div>
-            
+
+                <div style={{ top: '15rem', left: '6rem' }}
+                    data-aos="zoom-out-left">
+                    <Card
+                        image={vector1}
+                        heading={'Data Science'}
+                        details={"lorem ipsum dolor sit amet"}
+                    />
+                </div>
+
             </div>
         </div>
 
